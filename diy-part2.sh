56048c641b536e925f4d2813b3a5c2dcf742d3d1
ouyang789987 <ouyang789987@163.com>
2023-09-14 18:08:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+make menuconfig
