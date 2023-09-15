383be52459a7ec2126942587dc276f1b4dc35d06
ouyang789987 <ouyang789987@163.com>
2023-09-15 14:27:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,9 +19,17 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 make defconfig
 echo "CONFIG_PACKAGE_luci-app-oaf=y" >>.config  
 
-#如果要编译树莓派4，需要指定架构和子架构及产品型号
-echo 'CONFIG_TARGET_ARCH="arm"' >>.config  
-echo 'CONFIG_TARGET_BOARD="bcm27xx"' >>.config  
-echo 'CONFIG_TARGET_SUBTARGET="bcm2711"' >>.config  
+#如果要编译树莓派4，需要指定架构和子架构及产品型号，最好还是通过make menuconfig 在界面中选择比较好
+
+#CONFIG_TARGET_bcm27xx=y
+#CONFIG_TARGET_bcm27xx_bcm2711_DEVICE_rpi-4=y
+#CONFIG_TARGET_bcm27xx_bcm2711=y
+#CONFIG_TARGET_bcm27xx_bcm2711_DEVICE_rpi-4=y
+#CONFIG_TARGET_BOARD="bcm27xx"
+#CONFIG_TARGET_SUBTARGET="bcm2711"
+#CONFIG_TARGET_PROFILE="DEVICE_rpi-4"
+#CONFIG_TARGET_ARCH_PACKAGES="aarch64_cortex-a72"
+#CONFIG_DEFAULT_TARGET_OPTIMIZATION="-Os -pipe"
+#CONFIG_CPU_TYPE="cortex-a72"
 
 make defconfig  
