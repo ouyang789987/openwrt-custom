7be768af1afa9d44c776019c57356800c26e016f
ouyang789987 <ouyang789987@163.com>
2023-09-15 09:48:22 +0800

    Update diy-part2.sh
    添加了如果要编译特定平台设备时需要指定的参数

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,4 +18,10 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #make menuconfig
 make defconfig
 echo "CONFIG_PACKAGE_luci-app-oaf=y" >>.config  
+
+#如果要编译树莓派4，需要指定架构和子架构及产品型号
+echo 'CONFIG_TARGET_ARCH="arm"' >>.config  
+echo 'CONFIG_TARGET_BOARD="bcm27xx"' >>.config  
+echo 'CONFIG_TARGET_SUBTARGET="bcm2711"' >>.config  
+
 make defconfig  
